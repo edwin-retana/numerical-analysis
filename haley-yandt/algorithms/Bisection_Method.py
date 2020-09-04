055672b0b003f1a851bf7bc94d89e2f1e728fbd2
Haley Yandt <yandthj@Haleys-MacBook-Pro.local>
2020-09-03 20:08:05 -0700

    Adding Newton's Method

--- haley-yandt/algorithms/Bisection_Method.py
+++ haley-yandt/algorithms/Bisection_Method.py
@@ -1,11 +1,19 @@
 #%%
+import sympy as sym
 
+#%%
 #example function 
 def f(x):
-    #return x*x*x*x - 2*x*x*x - 4*x*x + 4*x + 4 
-    return x*x*x - x*x + 2
+    return x**3 - 2*x**2 - 5
+    #return x*x*x - x*x + 2
 
 
+def derivative(f, var, n):
+    x = sym.symbols('x')
+    expr = sym.diff(f, x, n) 
+    return expr.subs(x, var)
+
+#%%
 #n is number of allowed iterations, t is tolerance 
 def bisectionMethod(a, b, n, t):
     i = 1
@@ -28,5 +36,22 @@ def bisectionMethod(a, b, n, t):
     print("method failed")
             
 #%%
-bisectionMethod(-200, 300, 100,  0.01)
+bisectionMethod(2, 3, 1000,  0.1)
 # %%
+def NewtonMethod(p_initial, t, n):
+    i = 1
+    x = sym.symbols('x')
+    while (i <= n):
+        p = p_initial - (f(p_initial) / derivative(f(x), p_initial, 1))
+        p = float(p)
+        if (abs(p - p_initial) < t):
+            print('the root is', p)
+            return 
+        i = i + 1
+        p_initial = p
+    print('method failed, too many iterations')
+    return 
+
+NewtonMethod(2, 0.0001, 1000)
+
+#%%
